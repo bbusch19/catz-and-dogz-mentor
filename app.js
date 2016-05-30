b82ebbd30407372169af05c0daa67453e3833f44
Breiden Busch <breiden.busch@gmail.com>
2016-05-30 11:16:46 -0600

    Halfway, and then they changed the project

--- app.js
+++ app.js
@@ -4,7 +4,27 @@ angular.module('rainingCatzAndDogz', ['ui.router'])
   .state('home',{
     url:'/',
     templateUrl: "./templates/homeTemp.html"
-  });
+  })
+  .state('catz', {
+      url: '/catz',
+      controller: 'catCtrl',
+      templateUrl: './templates/catzTemp.html'
+  })
+  .state('dogz', {
+      url: '/dogz',
+      controller: 'dogCtrl',
+      templateUrl: './templates/dogzTemp.html'
+  })
+  .state('cat',{
+      url: '/catz/:id',
+      controller: 'catCtrl',
+      templateUrl: './templates/catTemp.html'
+  })
+  .state('dog', {
+      url: '/dogz/:id',
+      controller: 'dogCtrl',
+      templateUrl: './templates/dogTemp.html'
+  })
 
   $urlRouterProvider
   .otherwise('/');
